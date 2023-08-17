845c33849a3d44925b9ab43fcf2e9e35f4c8dda2
Leela's UBUNTU <leelakrishnachowdary21171@gmail.com>
2023-08-17 10:30:45 +0530

    vs

--- DBMS/NITCbase/mynitcbase/main.cpp
+++ DBMS/NITCbase/mynitcbase/main.cpp
@@ -1,36 +1,38 @@
-#include "Buffer/StaticBuffer.h"
-#include "Cache/OpenRelTable.h"
-#include "Disk_Class/Disk.h"
-#include "FrontendInterface/FrontendInterface.h"
-#include <iostream>	
-//  cd DBMS/NITCbase/mynitcbase
 int main(int argc, char *argv[]) {
-  /* Initialize the Run Copy of Disk */
-  // StaticBuffer buffer;
-  // OpenRelTable cache;
-  
+  //stage 2
   Disk disk_run;
-// Stage 1
-// vs code
-  // unsigned char buffer[BLOCK_SIZE];
-  // Disk::readBlock(buffer,7000);
-  // char message[]="hello";
-  // memcpy(buffer+20,message,6);
-  // Disk::writeBlock(buffer,7000);
-  
-  // unsigned char buffer2[BLOCK_SIZE];
-  // char message2[6];
-  // Disk::readBlock(buffer2,7000);
-  // memcpy(message2,buffer2+20,6);
-  // std::cout << message2;
-//Stage 1 Exercise
-    unsigned char buffer[BLOCK_SIZE];
-    Disk::readBlock(buffer,0);
-    for (int i = 0; i < 6; i++)
-	{
-		std::cout << (int)buffer[i] << " ";
+
+  // create objects for the relation catalog and attribute catalog
+  RecBuffer relCatBuffer(RELCAT_BLOCK);
+  RecBuffer attrCatBuffer(ATTRCAT_BLOCK);
+
+  HeadInfo relCatHeader;
+  HeadInfo attrCatHeader;
+
+  // load the headers of both the blocks into relCatHeader and attrCatHeader.
+  // (we will implement these functions later)
+  relCatBuffer.getHeader(&relCatHeader);
+  attrCatBuffer.getHeader(&attrCatHeader);
+
+  for (/* i = 0 to total relation count */) {
+
+    Attribute relCatRecord[RELCAT_NO_ATTRS]; // will store the record from the relation catalog
+
+    relCatBuffer.getRecord(relCatRecord, i);
+
+    printf("Relation: %s\n", relCatRecord[RELCAT_REL_NAME_INDEX].sVal);
+
+    for (/* j = 0 to number of entries in the attribute catalog */) {
+
+      // declare attrCatRecord and load the attribute catalog entry into it
+
+      if (/* attribute catalog entry corresponds to the current relation */) {
+        const char *attrType = attrCatRecord[ATTRCAT_ATTR_TYPE_INDEX].nVal == NUMBER ? "NUM" : "STR";
+        printf("  %s: %s\n", /* get the attribute name */, attrType);
+      }
+    }
+    printf("\n");
   }
-  
-  //return FrontendInterface::handleFrontend(argc, argv);
+
   return 0;
 }
