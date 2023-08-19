c2f0c0e5a4ea329e52786a47cd99047a7731fd90
Leela's UBUNTU <leelakrishnachowdary21171@gmail.com>
2023-08-19 11:38:52 +0530

    vs

--- DBMS/NITCbase/mynitcbase/Buffer/BlockBuffer.cpp
+++ DBMS/NITCbase/mynitcbase/Buffer/BlockBuffer.cpp
@@ -6,6 +6,7 @@
 
 BlockBuffer::BlockBuffer(int blockNum) {
   // initialise this.blockNum with the argument
+  this->blockNum=blockNum;
 }
 
 // calls the parent class constructor
@@ -16,13 +17,14 @@ int BlockBuffer::getHeader(struct HeadInfo *head) {
   unsigned char buffer[BLOCK_SIZE];
 
   // read the block at this.blockNum into the buffer
+  Disk::readBlock(buffer,this->blockNum);
 
   // populate the numEntries, numAttrs and numSlots fields in *head
   memcpy(&head->numSlots, buffer + 24, 4);
-  memcpy(&head->numEntries, /* fill this */, 4);
-  memcpy(&head->numAttrs, /* fill this */, 4);
-  memcpy(&head->rblock, /* fill this */, 4);
-  memcpy(&head->lblock, /* fill this */, 4);
+  memcpy(&head->numEntries,buffer+16, 4);
+  memcpy(&head->numAttrs,buffer+20, 4);
+  memcpy(&head->rblock,buffer+12, 4);
+  memcpy(&head->lblock,buffer+8, 4);
 
   return SUCCESS;
 }
@@ -32,18 +34,22 @@ int RecBuffer::getRecord(union Attribute *rec, int slotNum) {
   struct HeadInfo head;
 
   // get the header using this.getHeader() function
+  BlockBuffer::getHeader(&head);
 
   int attrCount = head.numAttrs;
   int slotCount = head.numSlots;
 
   // read the block at this.blockNum into a buffer
+  unsigned char buffer[BLOCK_SIZE];
+  Disk::readBlock(buffer,this->blockNum);
 
   /* record at slotNum will be at offset HEADER_SIZE + slotMapSize + (recordSize * slotNum)
      - each record will have size attrCount * ATTR_SIZE
      - slotMap will be of size slotCount
   */
   int recordSize = attrCount * ATTR_SIZE;
-  unsigned char *slotPointer = /* calculate buffer + offset */;
+  unsigned char *slotPointer = buffer + (HEADER_SIZE + slotCount + (recordSize*slotNum));
+  /* calculate buffer + offset */;
 
   // load the record into the rec data structure
   memcpy(rec, slotPointer, recordSize);
--- DBMS/NITCbase/mynitcbase/main.cpp
+++ DBMS/NITCbase/mynitcbase/main.cpp
@@ -19,7 +19,7 @@ int main(int argc, char *argv[]) {
   relCatBuffer.getHeader(&relCatHeader);
   attrCatBuffer.getHeader(&attrCatHeader);
 
-  for (/* i = 0 to total relation count */) {
+  for (int i=0;i<relCatHeader.numEntries;i++) {
 
     Attribute relCatRecord[RELCAT_NO_ATTRS]; // will store the record from the relation catalog
 
@@ -27,13 +27,17 @@ int main(int argc, char *argv[]) {
 
     printf("Relation: %s\n", relCatRecord[RELCAT_REL_NAME_INDEX].sVal);
 
-    for (/* j = 0 to number of entries in the attribute catalog */) {
+    for (int j=0;j<attrCatHeader.numEntries;j++) {
 
       // declare attrCatRecord and load the attribute catalog entry into it
+      Attribute attrCatRecord[ATTRCAT_NO_ATTRS];
 
-      if (/* attribute catalog entry corresponds to the current relation */) {
+      attrCatBuffer.getRecord(attrCatRecord,j);
+
+
+      if (!strcmp(attrCatRecord[ATTRCAT_REL_NAME_INDEX].sVal,relCatRecord[RELCAT_REL_NAME_INDEX].sVal)) {
         const char *attrType = attrCatRecord[ATTRCAT_ATTR_TYPE_INDEX].nVal == NUMBER ? "NUM" : "STR";
-        printf("  %s: %s\n", /* get the attribute name */, attrType);
+        printf("  %s: %s\n", attrCatRecord[ATTRCAT_ATTR_NAME_INDEX].sVal, attrType);
       }
     }
     printf("\n");
