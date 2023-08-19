a10d2b8aaa5e063255f8b606d3e08bbbbd2b8a37
Leela's UBUNTU <leelakrishnachowdary21171@gmail.com>
2023-08-19 09:41:56 +0530

    vs

--- DBMS/NITCbase/mynitcbase/Buffer/BlockBuffer.cpp
+++ DBMS/NITCbase/mynitcbase/Buffer/BlockBuffer.cpp
@@ -2,3 +2,51 @@
 
 #include <cstdlib>
 #include <cstring>
+// the declarations for these functions can be found in "BlockBuffer.h"
+
+BlockBuffer::BlockBuffer(int blockNum) {
+  // initialise this.blockNum with the argument
+}
+
+// calls the parent class constructor
+RecBuffer::RecBuffer(int blockNum) : BlockBuffer::BlockBuffer(blockNum) {}
+
+// load the block header into the argument pointer
+int BlockBuffer::getHeader(struct HeadInfo *head) {
+  unsigned char buffer[BLOCK_SIZE];
+
+  // read the block at this.blockNum into the buffer
+
+  // populate the numEntries, numAttrs and numSlots fields in *head
+  memcpy(&head->numSlots, buffer + 24, 4);
+  memcpy(&head->numEntries, /* fill this */, 4);
+  memcpy(&head->numAttrs, /* fill this */, 4);
+  memcpy(&head->rblock, /* fill this */, 4);
+  memcpy(&head->lblock, /* fill this */, 4);
+
+  return SUCCESS;
+}
+
+// load the record at slotNum into the argument pointer
+int RecBuffer::getRecord(union Attribute *rec, int slotNum) {
+  struct HeadInfo head;
+
+  // get the header using this.getHeader() function
+
+  int attrCount = head.numAttrs;
+  int slotCount = head.numSlots;
+
+  // read the block at this.blockNum into a buffer
+
+  /* record at slotNum will be at offset HEADER_SIZE + slotMapSize + (recordSize * slotNum)
+     - each record will have size attrCount * ATTR_SIZE
+     - slotMap will be of size slotCount
+  */
+  int recordSize = attrCount * ATTR_SIZE;
+  unsigned char *slotPointer = /* calculate buffer + offset */;
+
+  // load the record into the rec data structure
+  memcpy(rec, slotPointer, recordSize);
+
+  return SUCCESS;
+}
